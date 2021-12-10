87bf04c4142fa6ff96fe15b0bdaa6538ab3cc0ae
Andrii <andrii.zilnyk@gmail.com>
2021-12-10 10:12:17 +0200

    fixd dynamic import

--- 02_dynamic_import.js
+++ 02_dynamic_import.js
@@ -3,8 +3,8 @@
 async function f() {
     try {
         if (1) {
-            const { addNumbers } = await import('./dynamicmodule');
-            addNumbers(3, 4, 5);
+            const { addNumbers } = await require('./dynamicmodule');
+            console.log(addNumbers(3, 4, 5)); // 12
         }
     } catch (err) {
         console.log(err);
