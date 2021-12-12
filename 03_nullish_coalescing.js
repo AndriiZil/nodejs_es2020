425b525a71b6f0601713daad34914c591cf1e9a4
Andrii <andrii.zilnyk@gmail.com>
2021-12-12 11:57:58 +0200

    changes in nullish coalescing

--- 03_nullish_coalescing.js
+++ 03_nullish_coalescing.js
@@ -1,10 +1,14 @@
-console.log(false ?? 'some truthy value'); // false
+console.log('Result', false ?? 'some truthy value'); // false
 
-console.log(undefined ?? 'some truthy value'); // some truthy value
+console.log('Result', undefined ?? 'some truthy value'); // some truthy value
 
-console.log(null ?? 'some truthy value'); // some truthy value
+console.log('Result', null ?? 'some truthy value'); // some truthy value
 
-console.log(NaN ?? 'some truthy value'); // NaN
+console.log('Result', NaN ?? 'some truthy value'); // NaN
+
+console.log('Result', [] ?? 'some truthy value'); // []
+
+console.log('Result', {} ?? 'some truthy value'); // {}
 
 // ----------------------------------------------------------------
 
